fca147e7f3bbccc7da2aea92ae99f86d8f38858c
mahabbat <ma-xa-bat@mail.ru>
2023-10-27 11:23:35 +0300

    OOP_Seminar7

---
+++ src/HomeWork7/Observer.java
@@ -0,0 +1,6 @@
+package HomeWork7;
+public interface Observer {
+
+    void receiveOffer(String nameCompany, String vacancy, int salary);
+
+}
---
+++ src/HomeWork7/Publisher.java
@@ -0,0 +1,9 @@
+package HomeWork7;
+public interface Publisher {
+
+    void registerObserver(Observer observer);
+    void removeObserver(Observer observer);
+
+    void sendOffer(String companyName, String vacancy, int salary);
+
+}
